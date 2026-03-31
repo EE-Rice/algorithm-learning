8d66bcc910c9ef8633511775c647a99f1edc6b69
EE-Rice <kc202206@163.com>
2026-03-31 18:01:34 +0800

    fix:修复minIndex初始化位置错误

--- src/learn/sort/selectionSort.java
+++ src/learn/sort/selectionSort.java
@@ -6,7 +6,8 @@ public class selectionSort {
             return;
         }
 
-        for (int minIndex = 0, i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
