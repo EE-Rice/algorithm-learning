800753cb2c97af23ed49c27ee92d18984020a5bd
EE-Rice <kc202206@163.com>
2026-04-12 20:45:56 +0800

    feat: 最小栈

--- src/learn/queue_stack/MinStack.java
+++ src/learn/queue_stack/MinStack.java
@@ -1,4 +1,76 @@
 package learn.queue_stack;
 
+import java.util.Stack;
+
 public class MinStack {
+
+    // 测试链接：https://leetcode.cn/problems/min-stack/
+    class MinStack1 {
+        public Stack<Integer> data;
+        public Stack<Integer> min;
+
+        public MinStack1() {
+            data = new Stack<>();
+            min = new Stack<>();
+        }
+
+        public void push(int val) {
+            data.add(val);
+            if (min.isEmpty() || val <= min.peek()) {
+                min.push(val);
+            } else {
+                min.push(min.peek());
+            }
+        }
+
+        public void pop() {
+            data.pop();
+            min.pop();
+        }
+
+        public int top() {
+            return data.peek();
+        }
+
+        public int getMin() {
+            return min.peek();
+        }
+    }
+
+    class MinStack2 {
+        public final int MAXN = 8001;
+
+        public int[] data;
+        public int[] min;
+        int size;
+
+        public MinStack2() {
+            data = new int[MAXN];
+            min = new int[MAXN];
+            size = 0;
+        }
+
+        public void push(int val) {
+            data[size] = val;
+            if (size == 0 || val <= min[size - 1]) {
+                min[size] = val;
+            } else  {
+                min[size] = min[size - 1];
+            }
+            size++;
+        }
+
+        public void pop() {
+            size--;
+        }
+
+        public int top() {
+            return data[size - 1];
+        }
+
+        public int getMin() {
+            return min[size - 1];
+        }
+
+    }
 }
